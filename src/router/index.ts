0397fd31f29a2c4f88cdad2ac0f91236e7bb8da3
许缘伟 <1148090955@qq.com>
2022-03-17 09:35:07 +0800

    路由自动加载模块

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@
  * @Author: BlackJoken
  * @Date: 2022-01-11 10:02:32
  * @LastEditors: BlackJoken
- * @LastEditTime: 2022-01-12 11:15:29
+ * @LastEditTime: 2022-03-17 09:31:47
  */
 import { createRouter, createWebHashHistory } from 'vue-router'
 import test from '@/views/test.vue'
@@ -10,9 +10,22 @@ import owt from '@/views/owtAli.vue'
 import webrtc from '@/views/webrtc.vue'
 import srsdemo from '@/views/srsdemo.vue'
 
+
+
+/** 自动加载其他路由模块 */
+const registerRoute = () => {
+  const routerList = []
+  const modules = import.meta.globEager('./*.ts')
+  for (const val of Object.values(modules)) routerList.push(val.default)
+  return routerList.flat()//用于将嵌套的数组拉平 [1, 2, [3, 4]].flat()// [1, 2, 3, 4] 
+}
+/** 自动加载其他路由模块end */
+
 const Router = createRouter({
   history: createWebHashHistory(),
-  routes: [{
+  routes: [
+    ...registerRoute(),
+    {
     path: '/test',
     name: 'test',
     component: test
